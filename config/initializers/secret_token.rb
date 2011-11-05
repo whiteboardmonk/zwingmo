e242b81821a38f77b4823baf722db93cf887f853
Akshay Surve <akshay.surve@gmail.com>
2011-11-05 11:38:49 +0530

    Inital vanilla rails 3.0.5 bundle / dependencies / jquery / dev setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zwingmo::Application.config.secret_token = '03eefbbcddf7c7b7b238c02061b4cad7ffa02e5e36df37e24e2a0fd1bf83f9a060e269fee656023348da5cf9b8b0dcdd7c48543ac396f4340740f36118a65f37'
